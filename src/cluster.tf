720cc2714ceaa5890be94fccc33e997c5acc6442
Brian Denicola <brian@briandenicola.com>
2021-08-23 21:16:32 -0500

    Broke out into individual files

---
+++ src/cluster.tf
@@ -0,0 +1,102 @@
+resource "azurerm_private_dns_zone" "aks_private_zone" {
+  name                = "privatelink.${var.location}.azmk8s.io"
+  resource_group_name = azurerm_resource_group.k8s.name
+}
+
+resource "azurerm_private_dns_zone_virtual_network_link" "dns_link" {
+  name                  = "aks_dns_link"
+  resource_group_name   = azurerm_resource_group.k8s.name
+  private_dns_zone_name = azurerm_private_dns_zone.aks_private_zone.name
+  virtual_network_id    = data.azurerm_virtual_network.vnet.id
+}
+
+resource "azurerm_user_assigned_identity" "aks_identity" {
+  name                = "${var.cluster_name}-identity"
+  resource_group_name = azurerm_resource_group.k8s.name
+  location            = azurerm_resource_group.k8s.location
+}
+
+resource "azurerm_role_assignment" "aks_role_assignemnt_dns" {
+  scope                = azurerm_private_dns_zone.aks_private_zone.id
+  role_definition_name = "Private DNS Zone Contributor"
+  principal_id         = azurerm_user_assigned_identity.aks_identity.principal_id
+  skip_service_principal_aad_check = true
+}
+
+
+resource "azurerm_kubernetes_cluster" "k8s" {
+  depends_on = [
+    azurerm_role_assignment.aks_role_assignemnt_dns
+  ]
+  name                      = var.cluster_name
+  location                  = azurerm_resource_group.k8s.location
+  resource_group_name       = azurerm_resource_group.k8s.name
+  node_resource_group       = "${azurerm_resource_group.k8s.name}_nodes"
+  dns_prefix_private_cluster = var.cluster_name
+  kubernetes_version        = var.cluster_version
+  private_cluster_enabled   = "true"
+  private_dns_zone_id       = azurerm_private_dns_zone.aks_private_zone.id
+  automatic_channel_upgrade = "patch"
+
+  role_based_access_control {
+    enabled = "true"
+    azure_active_directory {
+      managed                 = "true" 
+      azure_rbac_enabled      = "true"  
+      admin_group_object_ids  = [
+        var.azure_rbac_group_object_id,
+      ] 
+    }
+  }
+
+  linux_profile {
+    admin_username = var.admin_user
+
+    ssh_key {
+      key_data = var.ssh_public_key
+    }
+  }
+
+  identity {
+    type                      = "UserAssigned"
+    user_assigned_identity_id = azurerm_user_assigned_identity.aks_identity.id
+  }
+
+
+  default_node_pool  {
+    name                    = "default"
+    node_count              = var.agent_count
+    availability_zones      = ["1", "2", "3"]
+    vm_size                 = var.vm_size
+    os_disk_size_gb         = 30
+    vnet_subnet_id          = data.azurerm_subnet.k8s_subnet.id
+    type                    = "VirtualMachineScaleSets"
+    enable_auto_scaling     = "true"
+    min_count               = 1
+    max_count               = 3
+    //local_account_disabled  = "false"
+  }
+
+  network_profile {
+    dns_service_ip     = var.dns_service_ip
+    service_cidr       = var.service_cidr
+    docker_bridge_cidr = "172.17.0.1/16"
+    network_plugin     = "azure"
+    load_balancer_sku  = var.load_balancer_sku
+    network_policy     = "calico"
+  }
+
+  addon_profile {
+    oms_agent {
+      enabled                    = true
+      log_analytics_workspace_id = azurerm_log_analytics_workspace.k8s.id
+    }
+    azure_policy {
+      enabled  = true
+    }
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
