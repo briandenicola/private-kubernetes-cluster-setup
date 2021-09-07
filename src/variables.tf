e4ccb1c222046287439fa2c06af0f40a1dae9b4f
Brian Denicola <brian@briandenicola.com>
2021-09-07 09:47:16 -0500

    Typos

--- src/variables.tf
+++ src/variables.tf
@@ -29,7 +29,7 @@ variable "cluster_name" {
 
 variable "cluster_version" {
   description = "The cluster version"
-  default     = "1.20.7"
+  default     = "1.14.8"
 }
 
 variable "resource_group_name" {
@@ -43,7 +43,7 @@ variable "agent_count" {
 
 variable "vm_size" {
   description = "The VM node size"
-  default     = "Standard_B4ms"
+  default     = "Standard_DS3_v2"
 }
 
 variable "admin_user" {
@@ -54,11 +54,6 @@ variable "admin_user" {
 variable "ssh_public_key" {
   description = "The public key for the local administrator"
   default     = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDGUfWYw+OI3udPmdcIklEeLapnR/9boHLNOpHwglZ+fxv959rjmXyq+ZB55xfHQqjYgvUARLbYmvnBgIpDDI95fo2tepHjspvw4nmM1OwRCt+DwY7Y7Rmq/5LRIj6RvJe0V2TsS8xE0VI907zLoatqQ6cO9kedlbr9KY4ZrRXYHOZWapHqcliyI29lZIPGdmAFjmtdkngmu4sgss9V+2gwWghp+bnMXyyn96oBxeQjCNDiP/90yucjYgoDPHslkLXc7jgdfnb+oxa0iG9bHutzgTdQ7ZkCZOnd++ZJISIvKhIIJAfqaQNVY1B7cXzFDcTJbZxpptZvKbaUaWhRS1uJ briandenicola@harpocrates.denicolafamily.com"
-}
-
-variable "log_analytics_workspace_name" {
-  description = "The name for the Log Analytics Workspace"
-  default     = "bjdloganalytics003"
 }
 
 variable "environment" {
