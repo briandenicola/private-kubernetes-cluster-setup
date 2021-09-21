d36da6387932c26dd4e8fda054bebb40c4bf426f
Brian Denicola <brian@briandenicola.com>
2021-09-21 10:15:05 -0500

    Moved jumpbox to own RG

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,3 +11,8 @@ resource "azurerm_resource_group" "k8s" {
   name                  = var.resource_group_name
   location              = var.location
 }
+
+resource "azurerm_resource_group" "jumpbox" {
+  name                  = "${var.resource_group_name}-jumpbox"
+  location              = var.location
+}
