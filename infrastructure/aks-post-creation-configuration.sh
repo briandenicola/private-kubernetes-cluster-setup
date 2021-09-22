65159647642f2cfd4426fbc2101cda058d9010c4
Brian Denicola <brian@briandenicola.com>
2021-09-22 12:00:56 -0500

    Removed opensevice mesh from config

--- infrastructure/aks-post-creation-configuration.sh
+++ infrastructure/aks-post-creation-configuration.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 az account set -s ${SUBSCRIPTION_ID}
-az aks enable-addons --addons open-service-mesh,azure-keyvault-secrets-provider -g ${RG} -n ${CLUSTER_NAME}
+az aks enable-addons --addons azure-keyvault-secrets-provider -g ${RG} -n ${CLUSTER_NAME}
 az aks update -g ${RG} -n ${CLUSTER_NAME} --enable-pod-identity
 
 RESOURCEID=`az identity show --name ${INGRESS_IDENTITY} --resource-group ${RG} --query id -o tsv`
