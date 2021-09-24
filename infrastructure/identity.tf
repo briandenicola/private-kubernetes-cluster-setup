0ae8b22a1cfd5a5349c1e6b56130683e24d27247
Brian Denicola <brian@briandenicola.com>
2021-09-23 20:48:55 -0500

    RBAC assignments for pod identities

--- infrastructure/identity.tf
+++ infrastructure/identity.tf
@@ -46,9 +46,3 @@ resource "azurerm_role_assignment" "acr_pullrole_nodepool" {
   skip_service_principal_aad_check = true
 }
 
-resource "azurerm_role_assignment" "aks_role_assignemnt_msi" {
-  scope                     = azurerm_user_assigned_identity.aks_kubelet_identity.id
-  role_definition_name      = "Managed Identity Operator"
-  principal_id              = azurerm_user_assigned_identity.aks_identity.principal_id
-  skip_service_principal_aad_check = true 
-}
---
+++ infrastructure/pod-identity-rbac.tf
@@ -0,0 +1,27 @@
+resource "azurerm_role_assignment" "aks_role_assignemnt_msi" {
+  scope                     = azurerm_user_assigned_identity.aks_kubelet_identity.id
+  role_definition_name      = "Managed Identity Operator"
+  principal_id              = azurerm_user_assigned_identity.aks_identity.principal_id
+  skip_service_principal_aad_check = true 
+}
+
+resource "azurerm_role_assignment" "aks_role_assignemnt_ingress" {
+  scope                     = azurerm_user_assigned_identity.aks_ingress_identity.id
+  role_definition_name      = "Managed Identity Operator"
+  principal_id              = azurerm_user_assigned_identity.aks_identity.principal_id
+  skip_service_principal_aad_check = true 
+}
+
+/*
+data "azurerm_user_assigned_identity" "aks_app_ee85e06" {
+  name                = "${var.app_ee85e06_name}"
+  resource_group_name = "${var.app_ee85e06_resource_group}"
+}
+
+resource "azurerm_role_assignment" "aks_role_assignemnt_app_ee85e06" {
+  scope                     = data.azurerm_user_assigned_identity.aks_app_ee85e06.id
+  role_definition_name      = "Managed Identity Operator"
+  principal_id              = azurerm_user_assigned_identity.aks_identity.principal_id
+  skip_service_principal_aad_check = true 
+}
+*/
