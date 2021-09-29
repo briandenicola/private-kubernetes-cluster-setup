4a30b9a150621654f875a1ed677d9d83516f94e1
Brian Denicola <brian@briandenicola.com>
2021-09-28 21:07:08 -0500

    Removing pod identity. Moving to az aks pod instead.

--- infrastructure/pod-identity-rbac.tf
+++ infrastructure/pod-identity-rbac.tf
@@ -11,15 +11,3 @@ resource "azurerm_role_assignment" "aks_role_assignemnt_ingress" {
   principal_id              = azurerm_user_assigned_identity.aks_identity.principal_id
   skip_service_principal_aad_check = true 
 }
-
-data "azurerm_user_assigned_identity" "chat_ee85e06" {
-  name                = "${var.chat_ee85e06_identity}"
-  resource_group_name = "${var.chat_ee85e06_resource_group}"
-}
-
-resource "azurerm_role_assignment" "aks_role_assignemnt_chat_ee85e06" {
-  scope                     = data.azurerm_user_assigned_identity.chat_ee85e06.id
-  role_definition_name      = "Managed Identity Operator"
-  principal_id              = azurerm_user_assigned_identity.aks_identity.principal_id
-  skip_service_principal_aad_check = true 
-}
