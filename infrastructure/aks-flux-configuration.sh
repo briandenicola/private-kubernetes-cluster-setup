6f6f737a5e81224044fafa2ef3bc466e0459438e
Brian Denicola <brian@briandenicola.com>
2021-10-31 09:06:16 -0500

    Moved to Flux configuration script

--- infrastructure/aks-flux-configuration.sh
+++ infrastructure/aks-flux-configuration.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-export CLUSTER_BOOTSTRAP_PATH=./cluster-manifests/${SERVICE_MESH_TYPE}
-export REPO_BRANCH=master
-export GITHUB_TOKEN=${GITHUB_ACCESS_TOKEN}
-
 az login --identity
 az account set -s ${SUBSCRIPTION_ID}
-az aks get-credentials -g ${RG} -n ${CLUSTER_NAME} --overwrite-existing
+az aks get-credentials -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --overwrite-existing
 kubelogin convert-kubeconfig -l msi
 
 ACR_PASSWORD=`az acr credential show -n ${ACR_NAME} --subscription ${ACR_SUBSCRIPTION_ID} --query "passwords[0].value" -o tsv | tr -d '\n'`
 
-kubectl create ns flux-system
-kubectl -n flux-system delete secret https-credentials
+kubectl create ns flux-system || true
+kubectl -n flux-system delete secret https-credentials || true
 kubectl -n flux-system create secret generic https-credentials --from-literal=username=${ACR_NAME} --from-literal=password=${ACR_PASSWORD}
 
-flux bootstrap github --owner=${GITHUB_ACCOUNT} --repository=${GITHUB_REPO} --path=${CLUSTER_BOOTSTRAP_PATH} --branch=${REPO_BRANCH} --personal
+flux bootstrap github --owner=${GITHUB_ACCOUNT} --repository=${GITHUB_REPO} --path=${CLUSTER_BOOTSTRAP_PATH} --branch=${REPO_BRANCH}  --personal=true --private=false
