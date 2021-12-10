26404347bc549224d407048e060a6a25c95d1181
Brian Denicola <brian@briandenicola.com>
2021-12-10 16:54:49 -0600

    Removed old comments

--- scripts/aks-flux-configuration.sh
+++ scripts/aks-flux-configuration.sh
@@ -5,9 +5,6 @@ az account set -s ${ARM_SUBSCRIPTION_ID}
 az aks get-credentials -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --overwrite-existing
 kubelogin convert-kubeconfig -l msi
 
-#TODO: Move to SPN Login and disable Admin credentials on ACR
-#ACR_PASSWORD=`az acr credential show -n ${ACR_NAME} --subscription ${ACR_SUBSCRIPTION_ID} --query "passwords[0].value" -o tsv | tr -d '\n'`
-
 kubectl create ns flux-system || true
 kubectl -n flux-system delete secret https-credentials || true
 kubectl -n flux-system create secret generic https-credentials --from-literal=username=${ACR_SPN_ID} --from-literal=password=${ACR_SPN_PASSWORD}
