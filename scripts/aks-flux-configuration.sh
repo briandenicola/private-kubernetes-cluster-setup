dbf93e2ea957409f058e130c4d15561407ccb548
Brian Denicola <brian@briandenicola.com>
2021-12-10 16:30:06 -0600

    Moving to SPN for ACR access

--- scripts/aks-flux-configuration.sh
+++ scripts/aks-flux-configuration.sh
@@ -6,10 +6,10 @@ az aks get-credentials -g ${CLUSTER_RG} -n ${CLUSTER_NAME} --overwrite-existing
 kubelogin convert-kubeconfig -l msi
 
 #TODO: Move to SPN Login and disable Admin credentials on ACR
-ACR_PASSWORD=`az acr credential show -n ${ACR_NAME} --subscription ${ACR_SUBSCRIPTION_ID} --query "passwords[0].value" -o tsv | tr -d '\n'`
+#ACR_PASSWORD=`az acr credential show -n ${ACR_NAME} --subscription ${ACR_SUBSCRIPTION_ID} --query "passwords[0].value" -o tsv | tr -d '\n'`
 
 kubectl create ns flux-system || true
 kubectl -n flux-system delete secret https-credentials || true
-kubectl -n flux-system create secret generic https-credentials --from-literal=username=${ACR_NAME} --from-literal=password=${ACR_PASSWORD}
+kubectl -n flux-system create secret generic https-credentials --from-literal=username=${ACR_SPN_ID} --from-literal=password=${ACR_SPN_PASSWORD}
 
 flux bootstrap github --owner=${GITHUB_ACCOUNT} --repository=${GITHUB_REPO} --path=${CLUSTER_BOOTSTRAP_PATH} --branch=${REPO_BRANCH}  --personal=true --private=false
