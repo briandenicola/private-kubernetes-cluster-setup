68e9e37e6eb6d5a28ad8bb3148a5394c171839ed
Brian Denicola <brian@briandenicola.com>
2022-01-11 23:18:31 -0600

    Removed jumpbox

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -11,9 +11,3 @@ resource "azurerm_resource_group" "k8s" {
   name                  = var.resource_group_name
   location              = var.location
 }
-
-resource "azurerm_resource_group" "jumpbox" {
-  name                  = "${var.resource_group_name}_jumpbox"
-  location              = var.location
-}
-
