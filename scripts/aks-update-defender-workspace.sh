9fbeb68d43c6d266906732fccc92bc922fba9b40
briandenicola <brian@briandenicola.com>
2022-01-24 14:28:28 -0600

    Bug fix

--- scripts/aks-update-defender-workspace.sh
+++ scripts/aks-update-defender-workspace.sh
@@ -1,4 +1,4 @@
-az login --identity
+#az login --identity
 az account set -s ${ARM_SUBSCRIPTION_ID}
 
 CLUSTER_DETAILS=`az aks list --query "[?name=='${CLUSTER_NAME}']"`
@@ -9,4 +9,5 @@ LOGANALYTICS_RESOURCEID=`echo ${CLUSTER_DETAILS} | jq -r ".[].addonProfiles.omsa
 
 BODY="{ \"location\": \"${CLUSTER_LOCATION}\", \"properties\": { \"securityProfile\": { \"azureDefender\": { \"enabled\": true, \"logAnalyticsWorkspaceResourceID\": \"${LOGANALYTICS_RESOURCEID}\"	}}}}"
 
-az rest --method put --url "${CLUSTER_RESOURCEID}?api-version=2021-10-01" --body ${BODY} --headers content-type=application/json
+
+az rest --method put --url "${CLUSTER_RESOURCEID}?api-version=2021-10-01" --body "${BODY}" --headers content-type=application/json
