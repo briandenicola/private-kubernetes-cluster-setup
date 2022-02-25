e085271f6079a497c5a4f3243ec78b3b4293360e
briandenicola <brian@briandenicola.com>
2022-02-24 22:20:40 -0600

    Added aks-preview extension to post-setup script

--- infrastructure/aks-post-creation-addons.sh
+++ infrastructure/aks-post-creation-addons.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+az extension add --name aks-preview   
 az account set -s ${SUBSCRIPTION_ID}
-az aks update -g ${RG} -n ${CLUSTER_NAME} --enable-pod-identity --enable-oidc-issuer
+
+az aks update -g ${RG} -n ${CLUSTER_NAME} --enable-pod-identity 
+az aks update -g ${RG} -n ${CLUSTER_NAME} --enable-oidc-issuer
