063164e3690a0e67e3ce66f7e6f36b92cf72aa7a
Brian Denicola <brian@briandenicola.com>
2022-04-05 10:35:39 -0500

    Delete aks-post-creation-addons.sh

--- infrastructure/aks-post-creation-addons.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-az extension add --name aks-preview   
-az account set -s ${SUBSCRIPTION_ID}
-
-az aks update -g ${RG} -n ${CLUSTER_NAME} --enable-pod-identity 
-az aks update -g ${RG} -n ${CLUSTER_NAME} --enable-oidc-issuer
