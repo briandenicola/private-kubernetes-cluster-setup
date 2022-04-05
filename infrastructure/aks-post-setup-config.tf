550c7cf143c990963c36b826e46824de927cb06c
Brian Denicola <brian@briandenicola.com>
2022-04-05 10:40:52 -0500

    Update and rename defender.tf to aks-post-setup-config.tf

--- infrastructure/aks-post-setup-config.tf
+++ infrastructure/aks-post-setup-config.tf
@@ -1,8 +1,11 @@
 resource "azurerm_resource_group_template_deployment" "this" {
   depends_on = [
-    null_resource.post_setup_config
+    azurerm_kubernetes_cluster.k8s,
+    azurerm_kubernetes_cluster_node_pool.eshop_app_node_pool
+    //azurerm_kubernetes_cluster_node_pool.eshop_app_node_pool,
+    //azurerm_kubernetes_cluster_node_pool.traduire_app_node_pool
   ]
-
+  
   name                = "defender-for-cloud-update"
   resource_group_name = azurerm_resource_group.k8s.name
   deployment_mode     = "Incremental"
@@ -33,6 +36,11 @@ resource "azurerm_resource_group_template_deployment" "this" {
           "name": "[parameters('aksCluster')]", 
           "location": "[resourceGroup().location]",
           "properties": {
+              "oidcIssuerProfile": {
+                "enabled": true 
+              "podIdentityProfile": {
+                "enabled": true
+              },
               "securityProfile": { 
                 "azureDefender": { 
                   "enabled": true, 
