d51fab1a32adb1a50951748df4d5300afaf1d6d8
briandenicola <brian@briandenicola.com>
2023-03-08 15:27:23 -0600

    Removed apps nodepool

--- infrastructure/aks-nodepools.tf
+++ infrastructure/aks-nodepools.tf
@@ -1,28 +1,3 @@
-resource "azurerm_kubernetes_cluster_node_pool" "default_app_node_pool" {
-  lifecycle {
-    ignore_changes = [
-      node_count
-    ]
-  }
-
-  name                  = "apps"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.k8s.id
-  vnet_subnet_id        = data.azurerm_subnet.k8s_nodes_subnet.id
-  pod_subnet_id         = data.azurerm_subnet.k8s_pods_subnet.id
-  vm_size               = "Standard_DS2_v2"
-  enable_auto_scaling   = true
-  mode                  = "User"
-  os_sku                = "CBLMariner"
-  os_disk_size_gb       = 30
-  node_count            = 3
-  min_count             = 3
-  max_count             = 6
-
-  upgrade_settings {
-    max_surge           = "25%"
-  }
-}
-
 resource "azurerm_kubernetes_cluster_node_pool" "eshop_app_node_pool" {
   lifecycle {
     ignore_changes = [
@@ -51,6 +26,31 @@ resource "azurerm_kubernetes_cluster_node_pool" "eshop_app_node_pool" {
 }
 
 /*
+resource "azurerm_kubernetes_cluster_node_pool" "default_app_node_pool" {
+  lifecycle {
+    ignore_changes = [
+      node_count
+    ]
+  }
+
+  name                  = "apps"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.k8s.id
+  vnet_subnet_id        = data.azurerm_subnet.k8s_nodes_subnet.id
+  pod_subnet_id         = data.azurerm_subnet.k8s_pods_subnet.id
+  vm_size               = "Standard_DS2_v2"
+  enable_auto_scaling   = true
+  mode                  = "User"
+  os_sku                = "CBLMariner"
+  os_disk_size_gb       = 30
+  node_count            = 3
+  min_count             = 3
+  max_count             = 6
+
+  upgrade_settings {
+    max_surge           = "25%"
+  }
+}
+
 resource "azurerm_kubernetes_cluster_node_pool" "traduire_app_node_pool" {
   lifecycle {
     ignore_changes = [
