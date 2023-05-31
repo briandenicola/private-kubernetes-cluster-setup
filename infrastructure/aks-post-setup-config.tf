8fa906a9b6e234076c3824d0d8780b88a26e051e
briandenicola <brian@briandenicola.com>
2023-05-31 07:57:16 -0500

    Remove public weekly maintenance schedule

--- infrastructure/aks-post-setup-config.tf
+++ infrastructure/aks-post-setup-config.tf
@@ -29,30 +29,3 @@ resource "azapi_update_resource" "this" {
     }
   })
 }
-
-data "azapi_resource" "weekend_utc" {
-  type = "Microsoft.Maintenance/publicMaintenanceConfigurations@2021-09-01-preview"
-  name = "aks-mrp-cfg-weekend_utc-6"
-  parent_id = "/subscriptions/${data.azurerm_subscription.current.subscription_id}"
-}
-
-resource "azapi_resource" "maintenance_window" {
-
-  depends_on = [
-    azurerm_kubernetes_cluster.k8s,
-    azurerm_kubernetes_cluster_node_pool.eshop_app_node_pool,
-    //azurerm_kubernetes_cluster_node_pool.default_app_node_pool,
-    //azurerm_kubernetes_cluster_node_pool.traduire_app_node_pool
-  ]
-
-  type = "Microsoft.Maintenance/configurationAssignments@2022-07-01-preview"
-  name = "maintenance-window"
-  location = var.location
-  parent_id = azurerm_kubernetes_cluster.k8s.id
-  body = jsonencode({
-    properties = {
-      maintenanceConfigurationId = data.azapi_resource.weekend_utc.id
-      resourceId =  azurerm_kubernetes_cluster.k8s.id
-    }
-  })
-}
