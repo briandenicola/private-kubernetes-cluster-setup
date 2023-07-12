7823db64f54fd95acf649b798077fc84b7b98001
briandenicola <brian@briandenicola.com>
2023-07-12 11:42:52 -0500

    Removed Pod Identities

--- infrastructure/aks-post-setup-config.tf
+++ infrastructure/aks-post-setup-config.tf
@@ -1,31 +1,16 @@
-resource "azapi_update_resource" "this" {
-  depends_on = [
-    azurerm_kubernetes_cluster.k8s,
-    azurerm_kubernetes_cluster_node_pool.eshop_app_node_pool,
-    //azurerm_kubernetes_cluster_node_pool.default_app_node_pool,
-    //azurerm_kubernetes_cluster_node_pool.traduire_app_node_pool
-  ]
+# resource "azapi_update_resource" "this" {
+#   depends_on = [
+#     azurerm_kubernetes_cluster.k8s,
+#     azurerm_kubernetes_cluster_node_pool.eshop_app_node_pool,
+#     //azurerm_kubernetes_cluster_node_pool.default_app_node_pool,
+#     //azurerm_kubernetes_cluster_node_pool.traduire_app_node_pool
+#   ]
 
-  type        = "Microsoft.ContainerService/managedClusters@2023-01-02-preview"
-  resource_id = azurerm_kubernetes_cluster.k8s.id
+#   type        = "Microsoft.ContainerService/managedClusters@2023-01-02-preview"
+#   resource_id = azurerm_kubernetes_cluster.k8s.id
 
-  body = jsonencode({
-    properties = {
-      podIdentityProfile = {
-        enabled = true
-      }
-      autoUpgradeProfile = {
-        nodeOSUpgradeChannel = "NodeImage"
-      } 
-      azureMonitorProfile = {
-        metrics = {
-          enabled = true,
-          kubeStateMetrics =  {
-            metricLabelsAllowlist = ""
-            metricAnnotationsAllowList = ""
-          }
-        }
-      }  
-    }
-  })
-}
+#   body = jsonencode({
+#     properties = {
+#     }
+#   })
+# }
